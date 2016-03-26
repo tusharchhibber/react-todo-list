4bd80c1c11eb63a7de80d6fb45b95320fdb6ff5e
tushar.chhibber <tushar.chhibber1@gmail.com>
2016-03-26 13:45:29 -0700

    Initial Setup

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
+import React from 'react'
+import {render} from 'react-dom'
+import App from '../components/App'
 
+render(
+	// define the encompassing component,
+	// DOM elenent we want to mount it to
+
+	<App/>,
+	document.getElementById('app')
+
+)
