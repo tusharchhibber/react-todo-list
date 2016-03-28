5910f869f1ffce6406d39548881ce07e2901090d
tushar.chhibber <tushar.chhibber1@gmail.com>
2016-03-28 16:26:49 -0700

    redux-thunk commit

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,15 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import rootReducer from './reducers'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
+
+// add all middleware here
+// eg: redux-logger, redux-thunk
+// thunk middleware will see if action passes an object or function
+// if object, it will execute it immediately otherwise asynchronously
 let finalCreateStore = compose(
-  applyMiddleware(logger())
+  applyMiddleware(thunk, logger())
 )(createStore)
 
 
